2030e5de57d1e4b72dbff91cbb3ef58d984a63e0
ethanparab <59212466+ethanparab@users.noreply.github.com>
2022-06-30 14:21:15 -0500

    yote the mixins

--- src/main/java/net/ddns/pinesmp/api/mixin/MinecraftServerMixin.java
+++
@@ -1,9 +0,0 @@
-package net.ddns.pinesmp.api.mixin;
-
-import net.ddns.pinesmp.api.util.MinecraftServerInterface;
-import net.minecraft.server.MinecraftServer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(MinecraftServer.class)
-public abstract class MinecraftServerMixin implements MinecraftServerInterface {
-}
