47e16e888a106c56556f49759eb89c886fd3c431
MandarinPine18 <59212466+MandarinPine18@users.noreply.github.com>
2022-07-09 14:14:53 -0500

    the test controller got a new mapping and I cleaned up the notation a little

--- src/main/java/net/pinesmp/pinesmpapi/net/TestController.java
+++ src/main/java/net/pinesmp/pinesmpapi/net/TestController.java
@@ -1,18 +1,24 @@
 package net.pinesmp.pinesmpapi.net;
 
 import net.pinesmp.pinesmpapi.util.Response;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
-@RestController
+@Controller
+@ResponseBody
+@RequestMapping(value = "/api/test", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TestController {
-	@GetMapping("/test")
-	public Response test(@RequestParam(value = "name", defaultValue = "World") String name) {
+	@RequestMapping(value = "")
+	public Response test() {
 		return new Response(true, new LinkedList<String>(), new LinkedList<String>(), new LinkedHashMap<String, Object>());
 	}
 }
