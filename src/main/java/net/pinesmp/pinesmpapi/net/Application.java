408d314318ba79dfd8732a9123690741195d327e
MandarinPine18 <59212466+MandarinPine18@users.noreply.github.com>
2022-07-09 14:11:27 -0500

    I'm trying to move away from Spring Boot as much as possible, so I've switched to Spring MVC to the extent I could

--- src/main/java/net/pinesmp/pinesmpapi/net/Application.java
+++ src/main/java/net/pinesmp/pinesmpapi/net/Application.java
@@ -1,17 +1,26 @@
 package net.pinesmp.pinesmpapi.net;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+@Configuration
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@ComponentScan
+@EnableWebMvc
+public class Application implements WebApplicationInitializer {
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class, TestController.class);
+	public void onStartup(@NotNull ServletContext container) {
+		ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet());
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/api/**");
 	}
 }
