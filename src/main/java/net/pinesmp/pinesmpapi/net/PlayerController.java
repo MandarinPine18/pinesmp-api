2bf9dea59971ee77fdb2450dd5ac82b078306244
MandarinPine18 <59212466+MandarinPine18@users.noreply.github.com>
2022-07-09 21:56:12 -0500

    /api/player -> /api/players

--- src/main/java/net/pinesmp/pinesmpapi/net/PlayerController.java
+++ src/main/java/net/pinesmp/pinesmpapi/net/PlayerController.java
@@ -25,7 +25,7 @@ public class PlayerController {
 		return server != null;
 	}
 
-	@RequestMapping("")
+	@RequestMapping("s")
 	public Response playerList(@RequestParam(name = "uuid", defaultValue = "false", required = false) boolean wantsUuid) {
 		// make sure the controller is ready
 		if (!isReady()) {
